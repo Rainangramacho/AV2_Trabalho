0885864043fe13ea0d12066f333266d6aba4ee1f
Rainangramacho <rai_bomfim@hotmail.com>
2019-05-30 01:47:17 -0300

    Falta Ajeitar concatenação
    Falta Ajeitar concatenação

--- CorrigirProvasClient.java
+++ CorrigirProvasClient.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
 public class CorrigirProvasClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         DatagramSocket socket;
         Scanner input = new Scanner(System.in);
 		int numQuestao [];
@@ -28,21 +29,23 @@ public class CorrigirProvasClient {
 		agenda.add("5;5;VVFVF");
 		
 		
-		int i;
- 
+		//int i;
+ /*
 		// [ C ] mostrando os "n" contatos da agenda (usando o índice)
 		// número de elementos da agenda: método size()
 		System.out.printf("Percorrendo o ArrayList (usando o índice)\n");
 		int n = agenda.size();
+		
 		for (i=0; i<n; i++) {
 		  System.out.printf("Posição %d- %s\n", i, agenda.get(i));
 		  String[] vetorExibir = agenda.toArray(new String[agenda.size()]);
 		  System.out.printf("teste: %s \n",vetorExibir[i]);
 		}
+ */
 		
 		
 		
-		/*
+		
 		
 		int i = 0;
 		int a = 0;
@@ -52,24 +55,42 @@ public class CorrigirProvasClient {
         
         int[] questoes = new int[qntQuestoes];
 		
-		for(i=0;i<5;i++){
+		for(i=0;i<qntQuestoes;i++){
 		System.out.print("Informe o número da questao: ");
 		numQuestao[i] = input.nextInt();
 		
 		System.out.print("Informe o número de alternativas dessa questao ");
 		numAlternativas[i] = input.nextInt();
 		
-		for(a=0;a<numAlternativas[i];a++){
-		System.out.println("Informe a resposta:  ");
-		respostas = input.nextLine();
-		}
+			for(a=0;a<numAlternativas[i];a++){
+			System.out.print("Informe a resposta:  ");
+			respostas[a] = input.next();
+			System.out.printf("Digitado:%s",respostas[a]);
+			resposta[i] += resposta[a];
+			
+			}
+			
+		System.out.printf("Teste:%s",resposta[i]);
+		
 		}
         
+		String str = "vvvfff";
+		char[] array = str.toCharArray();
+		StringBuilder result = new StringBuilder();
+		for (char c : array){
+			if (Character.isDigit(c)){
+				result.append(c);
+			}
+		}
+		System.out.println(result.toString());
+		
+		/*
             TODO: 
             Itera por todas as questões
             Insere a quantidade de alternativas
             Insere as alternativas
             Itera de novo e concatena todo mundo
-        */
+			*/
+        
     }
 }
